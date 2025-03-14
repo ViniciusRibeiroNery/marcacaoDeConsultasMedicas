3f7e705dcb093f2e869b12ddea8005b3d3c4c56c
ViniciusRibeiroNery <vininery2006@gmail.com>
2025-03-13 23:34:36 -0300

    Correções(Data: 2025/03/13).

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreens';
 import { SafeAreaView,StatusBar } from 'react-native';
+import HomeScreen from './screens/HomeScreens';
 
 const Stack = createNativeStackNavigator();
 
