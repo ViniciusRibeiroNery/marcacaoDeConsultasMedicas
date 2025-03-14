6de00f4a11c3ab71cbf5bb128277466bfd0b8b2b
ViniciusRibeiroNery <vininery2006@gmail.com>
2025-03-13 23:03:54 -0300

    Criando a tela principal em HomeScreen.tsx(Data: 2025/03/13).

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import styled from'styled-components/native';
 import { StatusBar } from 'react-native';
 
-export const HeaderContainer = styled.View'
+export const HeaderContainer = styled.View`
     background-color: #007BFF;
     padding-top: ${StatusBar.currentHeight}px;
     padding: 20px;
-';
+`;
 
-export const HeaderTitle = styled.Text'
+export const HeaderTitle = styled.Text`
     color: white;
     font-size: 20px;
     font-weight: bold;
-';
+`;
